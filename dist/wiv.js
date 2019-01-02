8b9b5af462c7f1f5580b85cd7473c4c126056664
Ben DeMeyer <ben@bendemeyer.com>
2019-01-01 21:37:07 -0800

    Moved find intersection code into generic function, some formatting adjustments

--- dist/wiv.js
+++ dist/wiv.js
@@ -27,7 +27,7 @@
     };
 
     function initWiv(wiv) {
-      //style wiv elements
+      // style wiv elements
       wiv.meta = {};
       wiv.style.display = "inline-block";
       let wivContent = document.createElement('div');
@@ -38,7 +38,7 @@
       wiv.meta.content = wivContent;
       wiv.appendChild(wivContent);
 
-      //insert wiv canvas element
+      // insert wiv canvas element
       let canvas = document.createElement('canvas');
       canvas.id = "wiv-curves-" + wivCounter++;
       canvas.className = "wiv-curves";
@@ -141,9 +141,9 @@
     }
 
     /**
-    Represents the logic to draw a single frame. Animates all wivs
-    */
-   function drawLines(canvas, {speed, height, tightness, thickness, increment, frame, color, image, imageSize, imageFrequency, ctx}={}) {
+     * Represents the logic to draw a single frame. Animates all wivs
+     */
+    function drawLines(canvas, {speed, height, tightness, thickness, increment, frame, color, image, imageSize, imageFrequency, ctx}={}) {
       var canvasImage = null;
       let imageMode = image !== undefined;
       if(imageMode){
@@ -157,11 +157,8 @@
       ctx.beginPath();
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-      //range of the sin function will be [-1 -> 1] * height. Since the logic will never want negative values for y (or clipping), it must have a vertical offset that takes all parameters into account
+      // range of the sin function will be [-1 -> 1] * height. Since the logic will never want negative values for y (or clipping), it must have a vertical offset that takes all parameters into account
       let offset = height + Math.max(thickness, imageMode ? imageSize : 0);
-
-      //let x = height * 2 + thickness
-      //let y = height - Math.sin(((x - frame) * tightness) * Math.PI / 180) * height + thickness;
 
       function calculateTopYValue(x) {
         return offset + (Math.sin(((x - frame) * tightness) * Math.PI / 180) * height);
@@ -172,154 +169,77 @@
       }
 
       function calculateBottomYValue(x) {
-        return (canvas.height - offset) + (Math.sin(((x - frame) * tightness) * Math.PI / 180) * height);
+        return (canvas.height - offset) + (Math.sin(((x + frame) * tightness) * Math.PI / 180) * height);
       }
 
       function calculateLeftXValue(y) {
-        return offset + Math.cos(((y - frame) * tightness) * Math.PI / 180) * height;
+        return offset + (Math.cos(((y + frame) * tightness) * Math.PI / 180) * height);
       }
 
-      function findTopLeftIntersection() {
-        let last = null;
-        for (let topX = height * 3; topX >= 0; topX--) {
-          let topY = calculateTopYValue(topX);
-          let leftX = calculateLeftXValue(topY);
-          let current = {
-            x: Math.round(topX), 
-            y: Math.round(topY)
-          };
-          if (leftX >= topX) {
-            if (last) {
-              return last;
-            } else {
-              return current;
-            }
+      function findIntersection(start, end, firstFunc, secondFunc, trim = 2) {
+        let reverse = (end < start);
+        let coordList = [];
+        for (let x = start; (reverse && x >= end) || (!reverse && x <= end); x += reverse ? -1 : 1) {
+          let y = firstFunc(x);
+          let newX = secondFunc(y);
+          coordList.unshift({
+            x: x,
+            y: y
+          });
+          coordList = coordList.slice(0, trim);
+          if ((reverse && newX >= x) || (!reverse && newX <= x)) {
+            return coordList.pop();
           }
-          last = current;
         }
-        return {
-          x: 0,
-          y: calculateTopYValue(0)
-        }
+        return coordList.pop();
       }
 
-      function findTopRightIntersection() {
-        let last = null;
-        for (let topX = canvas.width - (height * 3); topX <= canvas.width; topX++) {
-          let topY = calculateTopYValue(topX);
-          let rightX = calculateRightXValue(topY);
-          let current = {
-            x: Math.round(topX), 
-            y: Math.round(topY)
-          };
-          if (rightX <= topX) {
-            if (last) {
-              return last;
-            } else {
-              return current;
-            }
-          }
-          last = current;
-        }
-        return {
-          x: canvas.width,
-          y: calculateTopYValue(canvas.width)
-        }
-      }
-
-      function findBottomRightIntersection() {
-        let last = null;
-        for (let bottomX = canvas.width - (height * 3); bottomX <= canvas.width; bottomX++) {
-          let bottomY = calculateBottomYValue(bottomX);
-          let rightX = calculateRightXValue(bottomY);
-          let current = {
-            x: Math.round(bottomX), 
-            y: Math.round(bottomY)
-          };
-          if (rightX <= bottomX) {
-            if (last) {
-              return last;
-            } else {
-              return current;
-            }
-          }
-          last = current;
-        }
-        return {
-          x: canvas.width,
-          y: calculateBottomYValue(canvas.width)
-        }
-      }
-
-      function findBottomLeftIntersection() {
-        let last = null;
-        for (let bottomX = height * 3; bottomX >= 0; bottomX--) {
-          let bottomY = calculateBottomYValue(bottomX);
-          let leftX = calculateLeftXValue(bottomY);
-          let current = {
-            x: Math.round(bottomX), 
-            y: Math.round(bottomY)
-          };
-          if (leftX >= bottomX) {
-            if (last) {
-              return last;
-            } else {
-              return current;
-            }
-          }
-          last = current;
-        }
-        return {
-          x: 0,
-          y: calculateBottomYValue(0)
-        }
-      }
-
-      let topLeft = findTopLeftIntersection();
-      let topRight = findTopRightIntersection();
-      let bottomRight = findBottomRightIntersection();
-      let bottomLeft = findBottomLeftIntersection();
+      let topLeft = findIntersection(height * 4, 0, calculateTopYValue, calculateLeftXValue);
+      let topRight = findIntersection(canvas.width - height * 4, canvas.width, calculateTopYValue, calculateRightXValue);
+      let bottomRight = findIntersection(canvas.width - height * 4, canvas.width, calculateBottomYValue, calculateRightXValue);
+      let bottomLeft = findIntersection(height * 4, 0, calculateBottomYValue, calculateLeftXValue);
 
       let x, y;
 
-      //draw top
+      // draw top
       for (x = topLeft.x; x <= topRight.x; x += increment) {
         y = calculateTopYValue(x);
         imageMode && Math.floor(x % imageFrequency) == 0 && ctx.drawImage(canvasImage, x, y , imageSize, imageSize);
         ctx.lineTo(x, y);
       }
 
-      //draw right
+      // draw right
       for (; y <= bottomRight.y; y += increment) {
         x = calculateRightXValue(y);
         imageMode &&  Math.floor(y % imageFrequency) == 0 && ctx.drawImage(canvasImage, x , y , imageSize, imageSize);
         ctx.lineTo(x, y);
       }
 
-      //draw bottom
+      // draw bottom
       for (; x >= bottomLeft.x; x -= increment) {
         y = calculateBottomYValue(x);
         imageMode && Math.floor(x % imageFrequency) == 0 && ctx.drawImage(canvasImage, x, y  , imageSize, imageSize);
         ctx.lineTo(x, y);
       }
 
-      //draw left
+      // draw left
       for (; y >= topLeft.y; y -= increment) {
         x = calculateLeftXValue(y);
         imageMode && Math.floor(y % imageFrequency) == 0 && ctx.drawImage(canvasImage, x , y , imageSize, imageSize);
         ctx.lineTo(x, y);
       }
 
+      // complete connection
       ctx.lineTo(topLeft.x, topLeft.y);
 
-      //pull color from dataset
+      // pull color from dataset
       ctx.strokeStyle = color;
       ctx.lineWidth = thickness;
       if(thickness != 0 ){
         ctx.stroke();
       }
 
-      //current frame is tracked on per wiv basis. This is to help with speed calculations
+      // current frame is tracked on per wiv basis. This is to help with speed calculations
       if (frame > 100000) {
         frame = 0;
       }
